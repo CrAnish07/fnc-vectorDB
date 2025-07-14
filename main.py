9ee0bc142896c1f65f84cb0ef8b14dd8d435928c
CrAnish07 <mondalanish10@gmail.com>
2025-07-15 00:34:19 +0530

    main operational

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from config import settings
+from db.add_documents import add_docs
+from db.view_documents import view_docs
+from db.search_documents import search_docs_with_score, search_with_filter
+from db.update_documents import update_docs
+from db.delete_documents import delete_docs
+
 if __name__ == "__main__":
-    pass
+    add_docs()
+
+    print(view_docs())
+
+    print(search_docs_with_score("QUERY", k=2))
+    print(search_with_filter())
+
+    update_docs()
+    print(view_docs())
+
+    delete_docs()
+    print(view_docs())
